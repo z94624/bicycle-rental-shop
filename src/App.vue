b2b1e952ac5dc22ac73e916f6412c13c423296ee
smoBEE <z94624@gmail.com>
2023-08-03 11:06:18 +0800

    腳踏車出租店初始化(不含 Vuex)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="center">
+    <h1>- Bicycle Rental Shop -</h1>
+    <img style="width: 60px" src="@/assets/bicycle-icon.png" />
+    <p>Number of bicycles: {{ bicycleNum }}</p>
+    <button @click="plusOne" :disabled="isdisabledPlusFn">+1</button>&nbsp;
+    <button @click="minusOne" :disabled="isdisabledMinusFn">-1</button>
+    <p v-if="condition" style="color: olive">Bikes are available.</p>
+    <p v-else style="color: darkred">No bike is available.</p>
+    <div>
+      <hr />
+      <h3>Rules</h3>
+      <ol style="text-align: left; margin: 0 0 30px 60px">
+        <li v-for="(rule, index) in rules" :key="index">{{ rule }}</li>
+      </ol>
+    </div>
+    <TotalIncome v-bind:parentMsg="rentNum" @resetAll="resetAllData()" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TotalIncome from "@/components/TotalIncome.vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { TotalIncome },
+  data() {
+    return {
+      bicycleNum: 20,
+      rentNum: 0,
+      hourRentalFee: 100,
+      condition: true,
+      rules: [
+        "$100 each time.",
+        "Please return no later than 18:00.",
+        "Be safe.",
+      ],
+    };
+  },
+  computed: {
+    isdisabledPlusFn() {
+      if (this.bicycleNum == 20) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isdisabledMinusFn() {
+      if (this.bicycleNum == 0) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  methods: {
+    plusOne: function () {
+      this.bicycleNum = this.bicycleNum + 1;
+      if (this.bicycleNum == 0) {
+        this.condition = false;
+      } else {
+        this.condition = true;
+      }
+    },
+    minusOne: function () {
+      this.bicycleNum = this.bicycleNum - 1;
+      this.rentNum += 1;
+      if (this.bicycleNum == 0) {
+        this.condition = false;
+      } else {
+        this.condition = true;
+      }
+    },
+    resetAllData: function () {
+      this.bicycleNum = 20;
+      this.rentNum = 0;
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.center {
+  margin: auto;
+  width: 60%;
+  margin-top: 50px;
+  background-color: rgb(240, 240, 240);
+  padding: 20px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TotalIncome.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <hr />
+    <h3>Total Income</h3>
+    <p>100 X {{ parentMsg }} = {{ calculateIncome }}</p>
+    <div>
+      <button @click="reset()" style="margin-bottom: 30px">Reset All</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TotalIncome",
+  props: {
+    parentMsg: Number,
+  },
+  computed: {
+    calculateIncome: function () {
+      return this.parentMsg * 100;
+    },
+  },
+  methods: {
+    reset() {
+      this.$emit("resetAll");
+    },
+  },
+};
+</script>
